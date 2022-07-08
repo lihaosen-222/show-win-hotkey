2db581fe4098324ad2198980550138a3a0feff44
lihaosen <779427235@qq.com>
2022-07-08 23:21:48 +0800

    feat: 只置最前，添加提示

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # pip freeze > requirements.txt
 # pip install -r requirements.txt
+# Snake Case 下划线命名法
 
 virtualenv venv
--- test.py
+++ test.py
@@ -1,34 +1,35 @@
 import keyboard
 import win32gui
+import pythoncom
 from win32.lib import win32con
-
-
-# Snake Case 下划线命名法
-# pip freeze > requirements.txt
-# pip install -r requirements.txt
-
+from win32com import client
 
 win = {}
 
 
 def ctrl_shift(i):
     win[i] = win32gui.GetForegroundWindow()
+    print('绑定', win32gui.GetWindowText(win[i]))
 
 def ctrl(i):
     if(not win.get(i)): 
         return
+
     if win32gui.IsIconic(win[i]):
-        win32gui.ShowWindow(win[i], win32con.SW_SHOWNORMAL)
-        
+        win32gui.ShowWindow(win[i], win32con.SW_SHOWNORMAL) # 最小化的时候显示到最前
     else:
-        win32gui.ShowWindow(win[i], win32con.SW_SHOWMINIMIZED)
+        # 这四行不加 SetForegroundWindow 会报错，原理我也不知道
+        win32gui.BringWindowToTop(win[i])
+        pythoncom.CoInitialize()
+        shell = client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        win32gui.SetForegroundWindow(int(win[i])) # 没最小化的时候显示到最前
+    print('显示', win32gui.GetWindowText(win[i])) 
 
 
 for i in range(1, 6):
-    keyboard.add_hotkey('ctrl+'+str(i), ctrl, (i,))
-    keyboard.add_hotkey('ctrl+shift+'+str(i), ctrl_shift, (i,))
-# keyboard.add_hotkey('ctrl+q', ctrl_q)
-# keyboard.add_hotkey('ctrl+1', ctrl_1)
-# keyboard.add_hotkey('ctrl+2', ctrl_2)
-keyboard.wait('ctrl+shift+q')
+    keyboard.add_hotkey('alt+'+str(i), ctrl, (i,))
+    keyboard.add_hotkey('alt+shift+'+str(i), ctrl_shift, (i,))
+
+keyboard.wait('alt+shift+q')
 input('Press Enter to exit…')
